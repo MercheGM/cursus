674badc568b019e266384453faf106bfbab5b189
Merche GM <mergarciam@gmail.com>
2024-03-31 20:27:38 +0200

    Modified file ft_strlen

--- 01_Libft/libft/ft_strlen.c
+++ 01_Libft/libft/ft_strlen.c
@@ -17,18 +17,15 @@ size_t	ft_strlen(const char *s)
 	size_t	cont;
 
 	cont = 0;
-	while (*s)
-	{
-		s++;
+	while (*s++)
 		cont++;
-	}
 	return (cont);
 }
 
 /*int	main(void)
 {
-	char 	*str = "holi mundito";
+	char 	*str = "holi";
 	
-	printf("La cadena \"%s\" tiene %d caracteres\n", str, strlen(str));
+	//printf("La cadena \"%s\" tiene %d caracteres\n", str, strlen(str));
 	printf("La cadena \"%s\" tiene %d caracteres\n", str, ft_strlen(str));
-}	*/
+}*/
